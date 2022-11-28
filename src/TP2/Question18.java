86493e48ece375dce707010474a247b9bab43c6e
kawtaray16 <kawtar.lardjan@gmail.com>
2022-11-28 22:27:56 +0100

    Tentative question 18

---
+++ src/TP2/Question18.java
@@ -0,0 +1,38 @@
+package TP2;
+
+public class Question18 {
+
+	//Créer une class de bonus qui a une méthode d'ajout un bonus
+	
+	/*public class bonus {
+	 * public int ajouter_bounus(int n, Université U){ 
+	 * int s;
+	 * 
+	s=nbLivreMensuel_Autorise+n;
+	protected abstract verifier(Etudiant E, Universite U, s);
+	return s;
+	 * }
+	 */
+	
+	//Creer une classe hérite de class bounus 
+	/*public class bounus_Autorise extends bounus {
+	 *  vérifier (Etudiant E, Université U, s){
+	 * if (E.id == U.id){
+	 * if(s<U.forfait){
+	 * E.nbr_nbLivreMensuel_Autorise = s;
+	 * }
+	 * return System.out.println ("Votre forfait est augmenter a "+s);
+	 * }
+	 * 
+	 * */
+	
+/*public class bounus_non_Autorise extends bonus{
+	 * vérifier (Etudiant E, Université U, s){
+	 *  * if (E.id == U.id){
+	 * if (s> U.forfait){
+	 * E.nbr_nbLivreMensuel_Autorise = U.forfait;
+	 * }
+	 * return System.out.println("Votre forfait est au max "+s);
+	 * }
+	 */
+}
