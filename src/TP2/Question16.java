855d9292c6bac25e2347d0ea1d7e56f886617af5
kawtaray16 <kawtar.lardjan@gmail.com>
2022-11-28 22:24:52 +0100

    Tentative question16

---
+++ src/TP2/Question16.java
@@ -0,0 +1,49 @@
+package TP2;
+
+public class Question16 {
+//Créer une class de bonus qui a une méthode d'ajout un bonus
+	
+	/*public class bonus {
+	 * Etudiant E;
+	 * Universite U;
+	 * int s;
+	 * 
+	 * public int ajouter_bounus(int n, Université U){ 
+	 * 
+	 * int s;
+	s=nbLivreMensuel_Autorise+n;}
+	
+	void vérifier_bonusAutorise (Etudiant E, Université U, s){
+	 *  * if (E.id == U.id){
+	 * if (s> U.forfait){
+	 * E.nbr_nbLivreMensuel_Autorise = U.forfait;
+	 * }
+	 * return System.out.println("Votre forfait est au max "+s);
+	 * }
+	 * }
+	 *  void vérifier_bonus_non_autorise(Etudiant E, Universite U){
+	 *  * if (E.id == U.id){
+	 * if (s> U.forfait){
+	 * E.nbr_nbLivreMensuel_Autorise = U.forfait;
+	 * }
+	 * return System.out.println("Votre forfait est au max "+s);
+	 *  }
+	 * }
+	 */
+	
+	//Creer une classe hérite de class bounus 
+		/*public class bounus_Autorise extends bounus {
+		 *int s;
+		 * 
+		 *  s.vérifier_bonusAutorise ( E, U, s);
+		 * 
+		 * */
+	
+	/*public class bounus_non_Autorise extends bonus{
+	 *  s.vérifier_bonus_non_autorise(E, U);
+	 
+	 * }
+	 * 
+	 * */
+
+}
