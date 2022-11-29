6f40d7b8c617eae11f5454536fc401da3a35d955
kawtaray16 <kawtar.lardjan@gmail.com>
2022-11-29 17:21:03 +0100

    TentaticeParite4

---
+++ src/TP2/ControleurInscription.java
@@ -0,0 +1,48 @@
+package TP2;
+
+import javax.swing.JTextField;
+
+public class ControleurInscription extends ViewInscription{
+
+	//j'ai recuperer les valeurs (Modele)
+		String Matricule = JTextField.getText(txtMatricule);
+		String Nom = JTextField.getText(txtNom);
+		String Prenom = JTextField.getText(txtPrenom);
+		String email = JTextField.getText(txtEmail);
+		String pwd = JTextField.getText(txtPwd);
+		String id = JTextField.getText(txtIdentificationUniv);
+
+
+		//Affichage des valeurs pour vue
+				
+		System.out.println("Votre matricule est :" + Matricule + "\n");
+		System.out.println("Votre nom est :" + Nom + "\n");
+		System.out.println("Votre Prenom est :" + Prenom + "\n");
+		System.out.println("Votre email est :" + email + "\n");
+		System.out.println("Votre id_universite est :" + id + "\n");
+		
+		
+		
+		//Traiter les données par appel de fonctions verfier email et matricule (Controleur)
+		
+		Etudiant E = new EtudiantService();
+		boolean Eemail ;
+		boolean Ematricule;
+		Eemail = E.Exists(email);
+		Ematricule = E.Exists(Matricule);
+		if(Eemail == true) {
+			if(Ematricule == true) {
+				Object statement;
+				//inserer les variables dans la base de donnees
+				int statut = statement.executeUpdate( "INSERT INTO inscription (Matricule, nom, Prenom, email,"
+						+ "password,id_uni) VALUES ('Matricule','Nom','Prenom','email',"
+						+ " MD5('pwd'), 'id'");
+			}else {
+				System.out.println("La matricule déjà existe. Verifier votre matricule");
+			}
+			
+		}else {
+			System.out.println("L'email existe déjà. Vérifier votre email");
+		
+		}
+}}
---
+++ src/TP2/ViewInscription.java
@@ -0,0 +1,67 @@
+package TP2;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class ViewInscription extends JFrame{
+
+	  // Les zones de Textes
+    JTextField txtMatricule = new JTextField(10);
+    JTextField txtNom = new JTextField(10);
+    JTextField txtPrenom = new JTextField(10);
+    JTextField txtEmail = new JTextField(10);
+    JTextField txtPwd = new JTextField(10);
+    JTextField txtIdentificationUniv = new JTextField(10);
+	
+    // Création des labels 
+    JLabel lblMatricule = new JLabel("Matricule :", JLabel.RIGHT); 
+    lblMatricule.setLabelFor(txtMatricule); 
+    JLabel lblNom = new JLabel("Nom :", JLabel.RIGHT);
+    lblNom.setLabelFor(txtNom); 
+    JLabel lblPrenom = new JLabel("Prenom :", JLabel.RIGHT);
+    lblPrenom.setLabelFor(txtPrenom); 
+    JLabel lblEmail = new JLabel("Email :", JLabel.RIGHT);
+    lblEmail.setLabelFor(txtEmail); 
+    JLabel lblPwd = new JLabel("mot de passe :", JLabel.RIGHT);
+    lblPwd.setLabelFor(txtPwd); 
+   JLabel lblidentificationUniv = new JLabel("identification Universite : ", JLabel.RIGHT);
+   lblidentificationUniv.setLabelFor(txtidentificationUniv); 
+   
+   //Creation des deux bouttons Submit et cancel
+   
+   JButton bot1 = new JButton ("Cancel");
+   JButton bot2 = new JButton ("Submit");
+   
+   
+   
+   
+   //Creation de pannel
+   JPanel p = new JPanel( );
+   p.setLayout(new GridLayout(5, 4, 7, 7));
+   p.add(lblMatricule);
+   p.add(txtMatricule);
+   p.add(lblNom);
+   p.add(txtNom);
+   p.add(lblPrenom);
+   p.add(txtPrenom);
+   p.add(lblEmail);
+   p.add(txtEmail);
+   p.add(lblPwd);
+   p.add(txtPwd);
+   p.add(lblidentificationUniv);
+   p.add(txtidentificationUniv);
+   p.add(bot1);
+   p.add(bot2);
+   
+   //Creation de fenetre d'application
+   JFrame f = new JFrame("ViewInscription");
+   f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   f.setContentPane(p);
+   f.pack( );
+   f.setVisible(true);
+   
+}
+
